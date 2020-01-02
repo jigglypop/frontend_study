4d8fcef3bb3b056f7664d9ffdeb0e0e5b4b1ec7c
jigglypop <ydh2244@gmail.com>
2020-01-02 20:16:11 +0900

    0102

---
+++ 03_component/hello-react/src/Counter2.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Counter2 extends Component {
+  state = {
+    number: 0,
+    fixnumber: 10
+  };
+
+  render() {
+    const { number, fixnumber } = this.state;
+    return (
+      <div>
+        <h1>{number}</h1>
+        <h1>변하지 않는 값 {fixnumber}</h1>
+        <button
+          onClick={() => {
+            this.setState({ number: number + 1 });
+          }}
+        >
+          +1
+        </button>
+        <br></br>
+      </div>
+    );
+  }
+}
+
+export default Counter2;
