9e9939cc247aad0675c162d259f45ab6a19bac37
jigglypop <ydh2244@gmail.com>
2020-01-05 17:42:01 +0900

    0105

---
+++ 13_SPA/router-tutorial/src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import qs from "qs";
+
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+  const showDetail = query.detail === "true";
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>소개</p>
+      {showDetail && <p>detail true</p>}
+    </div>
+  );
+};
+
+export default About;
