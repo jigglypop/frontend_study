8d804daf03ccce1d9ed9b07527513f6c9fa103c9
jigglypop <ydh2244@gmail.com>
2020-01-10 16:55:34 +0900

    0110

---
+++ 24_frontend/blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -0,0 +1,4 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm } from '../../modules/auth';
+import AuthForm from '../../components/auth/AuthForm';
---
+++ 24_frontend/blog/blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WritePage = () => {
+  return <div>ì�½ê¸°</div>;
+};
+
+export default WritePage;
