e3448dd40ea09df704287fec79c8821e2a21067c
jigglypop <ydh2244@gmail.com>
2020-01-10 17:49:48 +0900

    0110

--- 24_frontend/blog/blog-frontend/src/containers/auth/LoginForm.js
+++ 24_frontend/blog/blog-frontend/src/containers/auth/LoginForm.js
@@ -2,3 +2,38 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { changeField, initializeForm } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const { form } = useSelector(({ auth }) => ({ form: auth.login }));
+
+  const onchange = e => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'login',
+        key: name,
+        value,
+      }),
+    );
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    dispatch(initializeForm('login'));
+  }, [dispatch]);
+
+  return (
+    <AuthForm
+      type="login"
+      form={form}
+      onChange={onchange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default LoginForm;
