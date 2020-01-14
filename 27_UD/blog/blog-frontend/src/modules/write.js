aa5b314aeceb6c5a66ced8246bbccdd88b121979
jigglypop <ydh2244@gmail.com>
2020-01-14 14:36:00 +0900

    0114

--- 27_UD/blog/blog-frontend/src/modules/write.js
+++ 27_UD/blog/blog-frontend/src/modules/write.js
@@ -5,13 +5,25 @@ import createRequestSaga, {
 import * as postsAPI from '../lib/api/posts';
 import { takeLatest } from 'redux-saga/effects';
 
-const INITIALIZE = 'write/INITIALIZE'; // 모든 내용 초기화
-const CHANGE_FIELD = 'write/CHANGE_FIELD'; // 특정 key 값 바꾸기
+const INITIALIZE = 'write/INITIALIZE';
+const CHANGE_FIELD = 'write/CHANGE_FIELD';
 const [
   WRITE_POST,
   WRITE_POST_SUCCESS,
   WRITE_POST_FAILURE,
-] = createRequestActionTypes('write/WRITE_POST'); // 포스트 작성
+] = createRequestActionTypes('write/WRITE_POST');
+const SET_ORIGINAL_POST = 'write/SET_ORIGINAL_POST';
+const [
+  UPDATE_POST,
+  UPDATE_POST_SUCCESS,
+  UPDATE_POST_FAILURE,
+] = createRequestActionTypes('write/UPDATE_POST');
+
+export const updatePost = createAction(
+  UPDATE_POST,
+  ({ id, title, body, tags }) => ({ id, title, body, tags }),
+);
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
 
 export const initialize = createAction(INITIALIZE);
 export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
@@ -24,10 +36,12 @@ export const writePost = createAction(WRITE_POST, ({ title, body, tags }) => ({
   tags,
 }));
 
-// saga 생성
+// Saga
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
+const updatePostSaga = createRequestSaga(UPDATE_POST, postsAPI.updatePost);
 export function* writeSaga() {
   yield takeLatest(WRITE_POST, writePostSaga);
+  yield takeLatest(UPDATE_POST, updatePostSaga);
 }
 
 const initialState = {
@@ -40,24 +54,33 @@ const initialState = {
 
 const write = handleActions(
   {
-    [INITIALIZE]: state => initialState, // initialState를 넣으면 초기상태로 바뀜
+    [INITIALIZE]: state => initialState,
     [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
       ...state,
-      [key]: value, // 특정 key 값을 업데이트
+      [key]: value,
     }),
     [WRITE_POST]: state => ({
       ...state,
-      // post와 postError를 초기화
       post: null,
       postError: null,
     }),
-    // 포스트 작성 성공
     [WRITE_POST_SUCCESS]: (state, { payload: post }) => ({
       ...state,
       post,
     }),
-    // 포스트 작성 실패
     [WRITE_POST_FAILURE]: (state, { payload: postError }) => ({
+      ...state,
+      postError,
+    }),
+    [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
+      ...state,
+      title: post.title,
+      body: post.body,
+      tags: post.tags,
+      originalPostId: post._id,
+    }),
+    [UPDATE_POST_SUCCESS]: (state, { payload: post }) => ({ ...state, post }),
+    [UPDATE_POST_FAILURE]: (state, { payload: postError }) => ({
       ...state,
       postError,
     }),
