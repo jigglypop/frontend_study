db24b0d2d5d1219caa478bc95402b767b07c6aa3
jigglypop <ydh2244@gmail.com>
2020-12-26 04:45:30 +0900

    1226

---
+++ 프로젝트/ssr1/src/index.server.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import express from "express";
+import { StaticRouter } from "react-router-dom";
+import App from "./App";
+import path from "path";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { rootSaga } from "./modules";
+import PreloadContext from "./lib/PreloadContext";
+import { END } from "redux-saga";
+import { ChunkExtractor, ChunkExtractorManager } from "@loadable/server";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { createPage } from "./lib/createPage";
+
+const statsFile = path.resolve("./build/loadable-stats.json");
+const app = express();
+const serverRender = async (req, res, next) => {
+  const context = {};
+  const sagaMiddleware = createSagaMiddleware();
+  const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk, sagaMiddleware))
+  );
+
+  const sagaPromise = sagaMiddleware.run(rootSaga).toPromise();
+  const preloadContext = {
+    done: false,
+    promises: [],
+  };
+
+  const extractor = new ChunkExtractor({ statsFile });
+  const jsx = (
+    <ChunkExtractorManager extractor={extractor}>
+      <PreloadContext.Provider value={preloadContext}>
+        <Provider store={store}>
+          <StaticRouter location={req.url} context={context}>
+            <App />
+          </StaticRouter>
+        </Provider>
+      </PreloadContext.Provider>
+    </ChunkExtractorManager>
+  );
+
+  ReactDOMServer.renderToStaticMarkup(jsx);
+  store.dispatch(END);
+  try {
+    await sagaPromise;
+    await Promise.all(preloadContext.promises);
+  } catch (e) {
+    return res.status(500);
+  }
+  preloadContext.done = true;
+  const root = ReactDOMServer.renderToString(jsx);
+  const stateString = JSON.stringify(store.getState()).replace(/</g, "\\u003c");
+  const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`;
+  const tags = {
+    scripts: stateScript + extractor.getScriptTags(),
+    links: extractor.getLinkTags(),
+    styles: extractor.getStyleTags(),
+  };
+  res.send(createPage(root, tags));
+};
+
+const serve = express.static(path.resolve("./build"), {
+  index: false,
+});
+
+app.use(serve);
+app.use(serverRender);
+
+app.listen(5000, () => {
+  console.log("Running on http://localhost:5000");
+});
